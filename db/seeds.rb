46580308b4f9a82ac41d05e579dd2f8710d1b86d
Ruslan Khait <rusty6916@gmail.com>
2022-07-01 16:48:55 -0400

    add resources routes for brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Brand.create(name:"Apothic", country:"United States", country_img: "https://wikiliq.org/wp-content/uploads/2021/02/United-States.png", img: "https://wikiliq.org/wp-content/uploads/2021/02/Apothic-wines-min.jpg", description:"Apothic Wines is a range of wines produced by California's largest wine exporter, E.J. Gallo. It began with a single wine, the Apothic Red, which was first released in commercial volumes in 2010. The wine is a blend of Zinfandel, Merlot, Syrah and Cabernet Sauvignon, made in a full-bodied style with ripe, fruit-forward characters and some residual sugar.", website:"https://www.apothic.com/")
